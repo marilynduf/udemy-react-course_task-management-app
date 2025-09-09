4becdd87cb9cf59b2044af4610cc752142588030
M <marilynduf@gmail.com>
2025-09-09 12:18:48 -0400

    Ajout composant SidebarProject

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import SidebarProject from "../src/composants/SidebarProject";
+
+function App() {
+    return (
+        <>
+            <h1 className="my-8 text-center text-5xl font-bold">
+                Marilyn's task managment app
+            </h1>
+            <SidebarProject />
+        </>
+    );
+}
+
+export default App;
---
+++ src/composants/SidebarProject.jsx
@@ -0,0 +1,8 @@
+export default function SidebarProject() {
+    return (
+        <section id="sidebar">
+            <h2>Your project</h2>
+            <div className="test"></div>
+        </section>
+    );
+}
