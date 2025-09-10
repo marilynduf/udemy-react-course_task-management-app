0b4d9521da3a7589d414db78825e4842886dc777
M <marilynduf@gmail.com>
2025-09-10 09:45:11 -0400

    Ajout valeur tag 'input' par defaut

--- src/composants/Input.jsx
+++ src/composants/Input.jsx
@@ -1,9 +1,9 @@
-export default function Input({ label, type, useTag = "input" }) {
+export default function Input({ ref, label, type, useTag = "input" }) {
     const UseTag = useTag;
     return (
         <div className="mb-8">
             <label htmlFor="">{label}</label>
-            <UseTag type={type} />
+            <UseTag ref={ref} type={type} />
         </div>
     );
 }
