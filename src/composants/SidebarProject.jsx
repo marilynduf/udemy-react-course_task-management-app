c7ef8e5e5af7a58f035b251cadc13098e64156d3
M <marilynduf@gmail.com>
2025-09-17 19:34:02 -0400

    Ajout fonctions qui permettent d'Afficher le composant Projet + ajout classes dynamiques

--- src/composants/SidebarProject.jsx
+++ src/composants/SidebarProject.jsx
@@ -1,5 +1,13 @@
 import Button from "./Button";
-export default function SidebarProject({ projectsData, onStartAddProject }) {
+import { useRef } from "react";
+export default function SidebarProject({
+    projectsData,
+    onStartAddProject,
+    onSelectProject,
+    selectedProjectId,
+}) {
+    const projectId = useRef();
+
     return (
         <aside id="sidebar" className="w-80 min-w-64 p-8">
             <h2>Your project</h2>
@@ -10,15 +18,25 @@ export default function SidebarProject({ projectsData, onStartAddProject }) {
                 + Add project
             </Button>
 
-            {projectsData.map((project) => (
-                <Button
-                    id={project.id}
-                    className="btn-project-list w-full text-left"
-                    key={project.id}
-                >
-                    {project.title}
-                </Button>
-            ))}
+            {projectsData.map((project) => {
+                let cssClasses = `btn-project-list w-full text-left px-2 py-1 rounded-sm hover:text-stone-200 hover:bg-stone-800`;
+                if (project.id === selectedProjectId) {
+                    cssClasses += " bg-stone-800 text-stone-200";
+                } else {
+                    cssClasses += " text-stone-400";
+                }
+                return (
+                    <Button
+                        ref={projectId}
+                        onClick={onSelectProject}
+                        id={project.id}
+                        className={cssClasses}
+                        key={project.id}
+                    >
+                        {project.title}
+                    </Button>
+                );
+            })}
         </aside>
     );
 }
